8175d3ab2bb2a7e1e7352a63874a45e9e0fa5215
renato3x <pereirarenato21@gmail.com>
2025-03-11 20:55:27 -0300

    chore: add dotenv support for environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CommonModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
