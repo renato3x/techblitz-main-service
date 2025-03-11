b38122420e073e75b698f8d2678db83e237c972c
renato3x <pereirarenato21@gmail.com>
2025-03-11 15:18:44 -0300

    build: build setup fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,69 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
+import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
-export default tseslint.config(
+/** @type {import('eslint').Linter.Config[]} */
+export default [
   {
-    ignores: ['eslint.config.mjs'],
+    files: ['**/*.ts'],
   },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
   {
     languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      ecmaVersion: 5,
-      sourceType: 'module',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+      globals: globals.node,
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    settings: {
+      prettier: {
+        semi: true,
+        singleQuote: true,
+        trailingComma: 'all',
+        tabWidth: 2,
+        printWidth: 120,
       },
     },
   },
   {
     rules: {
+      quotes: ['error', 'single', { avoidEscape: true }],
+      eqeqeq: ['error', 'always'],
+      semi: ['error', 'always'],
+      curly: ['error', 'all'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      'eol-last': ['error', 'always'],
+      'no-trailing-spaces': 'error',
+      'no-template-curly-in-string': 'error',
+      'prettier/prettier': [
+        'error',
+        {
+          semi: true,
+          singleQuote: true,
+          trailingComma: 'all',
+          tabWidth: 2,
+          printWidth: 120,
+        },
+      ],
     },
   },
-);
+];
