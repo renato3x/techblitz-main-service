7976f74247ab515c3ac14c23da7c7dfa25707418
renato3x <pereirarenato21@gmail.com>
2025-03-12 11:41:06 -0300

    chore: fixing app.module path alias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CommonModule } from '@common/common.module';
+import { CommonModule } from '@/common/common.module';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from '@user/user.module';
+import { UserModule } from '@/user/user.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), CommonModule, UserModule],
