947d91c55dcedd110e5cac13581be2b571157eb4
renato3x <pereirarenato21@gmail.com>
2025-04-19 23:02:35 -0300

    refactor: remove helper queues

--- src/common/services/message-queue-producer.service.ts
+++ src/common/services/message-queue-producer.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
-import amqplib from 'amqplib';
 
 @Injectable()
 export class MessageQueueProducerService implements OnApplicationBootstrap {
@@ -19,28 +18,9 @@ export class MessageQueueProducerService implements OnApplicationBootstrap {
         },
       },
     });
-
-    await this.defineHelperQueues();
   }
 
   async emit(pattern: string, data: any) {
     await firstValueFrom(this.client.emit(pattern, data));
-  }
-
-  private async defineHelperQueues() {
-    const connection = await amqplib.connect(process.env.BROKER_URL);
-    const channel = await connection.createChannel();
-
-    await channel.assertQueue(process.env.RETRY_QUEUE_NAME, {
-      durable: true,
-      arguments: {
-        'x-message-ttl': +process.env.RETRY_TIMEOUT,
-        'x-dead-letter-exchange': '',
-        'x-dead-letter-routing-key': process.env.QUEUE_NAME,
-      },
-    });
-
-    await channel.assertQueue(process.env.DEAD_LETTER_QUEUE_NAME, { durable: true });
-    await connection.close();
   }
 }
